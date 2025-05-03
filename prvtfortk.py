1af4c5693c13dcecd6dd23fe37bdff97ebe6b7aa
seriesturkey3 <46013534+seriesturkey3@users.noreply.github.com>
2025-05-03 16:24:29 +0300

    Update prvtfortk.py

--- prvtfortk.py
+++ prvtfortk.py
@@ -298,10 +298,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-hide_streamlit_style = """
-            <style>
-            [data-testid="stToolbar"] {visibility: hidden !important;}
-            footer {visibility: hidden !important;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
