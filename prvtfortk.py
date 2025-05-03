d4ab4287b62a784b29bf2706fa8510819eda2ba2
seriesturkey3 <46013534+seriesturkey3@users.noreply.github.com>
2025-05-03 16:15:51 +0300

    Update prvtfortk.py

--- prvtfortk.py
+++ prvtfortk.py
@@ -297,3 +297,12 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
